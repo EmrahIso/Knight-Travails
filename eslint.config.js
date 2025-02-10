4cec0d54e01afb17c413e4aa22b9755f38915e57
Emrah Isovic <emrahisovic20@gmail.com>
2025-02-10 17:25:15 +0100

    Inital project structure, included packages and linting

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+
+import globals from "globals";
+
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  // any other config imports go at the top
+  js.configs.recommended,
+  eslintPluginPrettierRecommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    files: ["src/**/*.js"],
+    ignores: ["**/*.config.js", "webpack.*.js"],
+    rules: {
+      semi: "error",
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+];
